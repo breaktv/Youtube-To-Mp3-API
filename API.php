e3646c49999e8b1b500ac0fa0e4b0fbbeae12f84
breaktv <33252264+breaktv@users.noreply.github.com>
2017-10-31 12:40:24 +0200

    Create API.php

---
+++ API.php
@@ -0,0 +1,3 @@
+
+<iframe style="width:100%;min-width:200px;max-width:350px;height:57px;border:0;overflow:hidden;" scrolling="no" src="//break.tv/widget/button/?link=<?php echo $_GET["v"]; ?>&color=DA4453&text=fff"></iframe>
+
